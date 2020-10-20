5818c2c03cc48b9c5d65e1ff8a1f2ebad979cf03
Oliver Meyer <oliver.meyer@maihiroproducts.com>
2020-10-21 00:39:19 +0200

    Increase version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 setup(
     name='clockysap',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     url='https://github.com/zudljk/clockysap',
     license='MIT',

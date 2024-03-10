5c01783326632249e35a7e1e410f347deb44ada3
Oliver Meyer <oliver.meyer@nagarro.com>
2024-03-10 20:26:19 +0100

    Fix errors in Firefox

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 setup(
     name='clockysap',
-    version='0.3',
+    version='0.5',
     packages=find_packages(),
     url='https://github.com/zudljk/clockysap',
     license='MIT',
